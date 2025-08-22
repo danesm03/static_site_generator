50174b4e3c73b86ab88bca7e437ec79f99d7160f
danesm03 <danesmithhh@gmail.com>
2025-08-22 11:56:10 -0500

    added copy_directory and helper function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,50 @@
-from textnode import TextNode, TextType
+from textnode import *
+from htmlnode import *
+from split_delimiter import *
+from markdown_blocks import *
+import os
+import shutil
+
+def remove_filetree(target_dir):
+    if os.path.exists(target_dir):
+        shutil.rmtree(f"{target_dir}/")
+
+def copy_directory(dir, target_dir, is_initial_call=True):
+    if is_initial_call:
+        remove_filetree(target_dir)
+
+
+    #check for dir and target dir existence
+    if not os.path.exists(dir):
+        raise Exception("directory does not exist")
+
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    # list the contents of the directory
+    directory_list = os.listdir(dir)
+    #target_dir_list = os.listdir(target_dir)
+    
+    
+    #recursive copying
+    for item in directory_list:
+        joined_path = os.path.join(dir, item)
+        if os.path.isdir(joined_path):
+            new_target = os.path.join(target_dir, item)
+            copy_directory(joined_path, new_target, is_initial_call=False)
+        if os.path.isfile(joined_path):
+            shutil.copy(joined_path, target_dir)
+        #print(f"target_list = {target_dir_list}, dir_list = {directory_list}")
+    
+
+
+
+    
 
 def main():
-    my_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(my_node)
-
-
+    copy_directory("static/", "public/")
 
 
 if __name__ == "__main__":
     main()
+
+    
